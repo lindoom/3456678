4fe4acf37e79644c685c3ef4611dbd1f6b554fbd
dimka <lindoom56@gmail.com>
2022-03-31 22:10:02 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict".
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+
+// console.log(answers);
+
+console.log('arr' + '5');
+
+    let incr = 10,
+        decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+
