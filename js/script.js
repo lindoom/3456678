28a878676855125f1d9e090c79b9a0754066b7d7
dimka <lindoom56@gmail.com>
2022-04-01 19:57:30 +0300

    01.04.22

--- js/script.js
+++ js/script.js
@@ -1,25 +1,21 @@
-"use strict".
+"use strict";
 
-// const answers = [];
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// answers[0] = prompt('Как ваше имя?', '');
-// answers[1] = prompt('Как ваша фамилия?', '');
-// answers[2] = prompt('Сколько вам лет?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько вы его оцените?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-// console.log(answers);
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
 
-console.log('arr' + '5');
-
-    let incr = 10,
-        decr = 10,
-        bukva = 10;
-
-incr++;
-decr--;
-
-console.log(incr);
-console.log(decr);
-console.log(bukva);
-
-
+      console.log(personalMovieDB);
